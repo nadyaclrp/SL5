a2cddf5f24e5ce1d1482ec87f800f3e674b2b2ac
nadyaclrp <nadya.clarine@gmail.com>
2023-05-02 22:55:55 +0700

    Making Master Page & Edit Profile (following Master )

---
+++ resources/views/master.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    {{-- import vite tailwind --}}
+    @vite('resources/css/app.css')
+
+    {{-- Fonts --}}
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <link rel="icon" type="image/x-icon" href="/assets/nadyaRemove.png">
+
+    <title>@yield('tittle')</title>
+
+    {{-- style --}}
+    <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <h3>@yield('titlePage')</h3>
+    @yield('body')
+</body>
+</html>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,26 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+@extends('master')
+@section('tittle','Profile')
 
-    {{-- import vite tailwind --}}
-    @vite('resources/css/app.css')
-
-    {{-- Fonts --}}
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-
-    <title>Profile</title>
-
-    <style>
-        body {
-            font-family: 'Poppins', sans-serif;
-        }
-    </style>
-
-</head>
-<body>
+@section('body')
     <section id="profile" class="pt-px w-full">
         {{-- landing page --}}
             <div class="bg-gradient-to-l from-lavenderGray text-navy w-full">
@@ -413,5 +394,4 @@
             </div>
 
     </section>
-</body>
-</html>
+@endsection
